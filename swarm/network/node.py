3a9010111eda9c587f9a0209b37d98444cfcb263
Danil <lkv97dn@gmail.com>
2019-01-11 05:45:47 +0300

    working communication between two.

--- swarm/network/node.py
+++ swarm/network/node.py
@@ -1,8 +1,10 @@
 
 from sys import argv
+import time
 import socket
 import socketserver
 from multiprocessing.dummy import Process as Thread, Pipe
+from .piped_server import PipedThreadedServer
 
 class Node:
 
@@ -24,24 +26,28 @@ class Node:
         def start(s):
             s._print("starting...")
             s._start_threads()
-
-        def _start_threads(s):
+            # this will be replaced by connection establishment
+            time.sleep(0.2)
             ret = s.func(s)
             s._print('user func returned',ret)
             return ret
+
+        def _start_threads(s):
+            s.server_pipe, pipe_end = Pipe(duplex=False)
+            s.server = PipedThreadedServer(s.port, pipe_end)
 
         def _print(s,*args):
             print("<<node %s"%s.name,*args)
 
         def _get_node_by_name(s,name):
             r = [n for n in s.network if n['name']==name]
-            assert len(r)==0, 'Wrong network info in node'+s.name
+            assert len(r)==1, 'Wrong network info in node'+s.name
             return r[0]
 
         def _send_to_node(s, data, node):
             host = node['host']
             port = node['port']
-            s._print('sending to'+host+':'+port)
+            s._print('sending to'+host+':'+str(port))
 
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 sock.connect((host, int(port) ))
@@ -53,4 +59,4 @@ class Node:
             s._send_to_node(data, node)
 
         def recv(s):
-            pass
+            return s.server_pipe.recv()
--- swarm/test_network.py
+++ swarm/test_network.py
@@ -3,14 +3,27 @@ from swarm_net import SwarmNetwork
 config = [
 
         { 'name':'A',
-            'host':'tis', },
+            'host':'localhost', },
         { 'name':'B',
-            'host':'tis', },
+            'host':'localhost', },
         { 'name':'C',
-            'host':'tis', }
+            'host':'localhost', }
         ]
+
+def _print(node,*arg):
+    print("###%s got"%node.name,*arg)
+
 def func(node):
     print("I am node",node.name,'with pid',node.pid)
+    if node.name=='A':
+        node.send('B','hello from a')
+    elif node.name=='B':
+        msg = node.recv()
+        _print(node,'got',msg)
+    else:
+        print("i'm the C and waiting")
+        msg = node.recv()
+
 
 def main():
     sw = SwarmNetwork()
