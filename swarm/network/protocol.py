707e08ab060f2a10d881fc65928827efc40fff27
Danil <lkv97dn@gmail.com>
2019-01-18 15:48:46 +0300

    added protocol api and cleaned node network file

--- swarm/network/protocol.py
+++ swarm/network/protocol.py
@@ -1,4 +1,4 @@
-import utils
+import network.net_utils as utils
 from enum import Enum
 
 class MessageType(Enum):
@@ -8,6 +8,38 @@ class MessageType(Enum):
 
     UNKNOWN = 255
 
+def form_data_message(data):
+    msg = bytes([MessageType.DATA.value]) + utils.int_to_bytes(len(data))
+    msg += bytes(data,'utf-8')
+    # TODO: pickle the data object
+    return msg
+
+def form_connect_message(name):
+    msg = bytes([MessageType.CONNECT.value]) + utils.int_to_bytes(len(name))
+    msg += bytes(name,'utf-8')
+    # TODO: pickle the data object
+    return msg
+
+def get_message_data(msg):
+    """
+    Parameters
+    ------------
+    msg: bytes
+        the input message
+    """
+    #TODO: use pickle
+    return msg[3:].decode('utf-8')
+
+def get_message_length(msg):
+    """
+    Parameters
+    ------------
+    msg: bytes
+        the input message
+    """
+    msg_len = utils.bytes_to_int(msg[1:3])
+    return msg_len
+
 def get_message_type(msg):
     """
     Parameters
@@ -15,7 +47,7 @@ def get_message_type(msg):
     msg: bytes
         the input message
     """
-    msg_type = msg_bytes[0]
+    msg_type = msg[0]
     if msg_type == 1:
         return MessageType.CONNECT
     elif msg_type == 2:
--- swarm/node_net.py
+++ swarm/node_net.py
@@ -1,11 +1,11 @@
 
 from sys import argv
 import time
-import socket
 import socketserver
 from multiprocessing.dummy import Process as Thread, Pipe
 from network.piped_server import PipedThreadedServer
 import network.net_utils as utils
+import network.protocol as protocol
 
 class Node:
         def __init__(s,
@@ -33,9 +33,6 @@ class Node:
 
                 s._print("starting...")
                 s._start_threads()
-                # this is to simulate delayed start
-                if s.name=="B":
-                        time.sleep(1)
                 s._handshake(s.network)
                 s._print("Connection with network established!")
                 s._print("##calling user fucntion")
@@ -43,39 +40,51 @@ class Node:
                 s._print('user func returned',ret)
                 return ret
 
+        def send(s, to, data):
+                node = s._get_node_by_name(to)
+                msg = protocol.form_data_message(data)
+                utils.send_to(msg, (node['host'], node['port']) )
+
+        def recv(s):
+                msg_bytes = s.server_pipe.recv()
+                #TODO: do not receive all data if mailformed message
+                # now we send oly strings
+                return protocol.get_message_data(msg_bytes)
+
         def _handshake(s, network):
-                """
-                Sends a connect signal to every node in network
-                then listens for response from every node in network
-                """
-                data = bytes([0]) + utils.int_to_bytes(len(s.name))
-                data += bytes(s.name,'utf-8')
-                network = [n for n in network if n['name']!=s.name]
-                names = [n['name'] for n in  network ]
+            """
+            Sends a connect signal to every node in network
+            then listens for response from every node in network
+            """
+            msg = protocol.form_connect_message(s.name)
+            network = [n for n in network if n['name']!=s.name]
+            names = [n['name'] for n in  network ]
 
-                for node in network:
-                        while True:
-                                try:
-                                        s._send_to_node( data, node)
-                                        break
-                                except ConnectionRefusedError as e:
-                                        #s._print('conn refused')
-                                        time.sleep(0.01)
+            for node in network:
+                while True:
+                    try:
+                        utils.send_to(msg, (node['host'],node['port']) )
+                        break
+                    except ConnectionRefusedError as e:
+                        #s._print('conn refused')
+                        time.sleep(0.01)
 
-                while len(names):
-                        msg = s.server_pipe.recv()
-                        msg_type = int(msg[0])
-                        if msg_type==0:
-                                name = msg[3:].decode('utf-8')
-                                s._print('got connection from',name)
-                                names.remove(name)
-                        else:
-                                s._print("warning: got unwanted message")
+            while len(names):
+                msg = s.server_pipe.recv()
+                msg_type = protocol.get_message_type(msg)
+
+                if msg_type==protocol.MessageType.CONNECT:
+                    name = protocol.get_message_data(msg)
+                    s._print('got connection from',name)
+                    names.remove(name)
+                else:
+                    s._print("warning: got unwanted message")
 
 
         def _start_threads(s):
                 s.server_pipe, pipe_end = Pipe(duplex=False)
                 s.server = PipedThreadedServer(s.port, pipe_end)
+
 
         def _print(s,*args):
                 print("<<node %s:"%s.name,*args)
@@ -84,33 +93,3 @@ class Node:
                 r = [n for n in s.network if n['name']==name]
                 assert len(r)==1, 'Wrong network info in node'+s.name
                 return r[0]
-
-        def _send_to_node(s, data, node):
-                host = node['host']
-                port = node['port']
-                #s._print('sending to'+host+':'+str(port))
-
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                        sock.connect((host, int(port) ))
-                        sock.sendall(data)
-                        sock.shutdown(socket.SHUT_RDWR)
-
-
-        def send(s, to, data):
-                node = s._get_node_by_name(to)
-                # TODO: pickle the data object
-
-                msg = bytes([3]) + utils.int_to_bytes(len(data))
-                msg += bytes(data,'utf-8')
-
-                s._send_to_node(msg, node)
-
-        def recv(s):
-                msg_bytes = s.server_pipe.recv()
-                #TODO: do not receive all data if mailformed message
-                msg_type = msg_bytes[0]
-                s._print('msg type',msg_type)
-                msg_len = utils.bytes_to_int(msg_bytes[1:3])
-                # now we send oly strings
-                # TODO: send pickle
-                return msg_bytes[3:].decode('utf-8')
